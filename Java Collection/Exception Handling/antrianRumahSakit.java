2e415cedbfc38809e0c252b035ccb51f7d513929
Dhikaaa234 <160991976+Dhikaaa234@users.noreply.github.com>
2024-05-16 21:17:34 +0700

    antrianRumahSakit.java

--- Java Collection/Exception Handling/antrianRumahSakit.java
+++ Java Collection/Exception Handling/antrianRumahSakit.java
@@ -3,7 +3,7 @@ import java.util.Queue;
 
 public class antrianRumahSakit {
     private Queue<String> antrianPasien;
-    private static final int Kapasitas_Max_Antrian = 10; 
+    private static final int kapasitasMaxAntrian = 10; 
 
     public antrianRumahSakit() {
         antrianPasien = new LinkedList<>();
@@ -11,40 +11,32 @@ public class antrianRumahSakit {
 
     public void tambahPasien(String namaPasien) {
         try {
-            if (antrianPasien.size() >= Kapasitas_Max_Antrian) {
-                throw new IllegalStateException("Kapasitas antrian telah mencapai batas maksimum");
+            if (antrianPasien.size() >= kapasitasMaxAntrian) {
+                throw new IllegalStateException("Antrian sudah mencapai batas maksimum. Tidak dapat menambahkan pasien baru.");
             }
 
-            try {
-                Integer.parseInt(namaPasien);
+            if (namaPasien.matches("\\d+")) {
                 throw new IllegalArgumentException("Nama pasien tidak boleh berupa angka");
-            } catch (NumberFormatException e) {}
+            }
 
             if (antrianPasien.add(namaPasien)) {
                 System.out.println(namaPasien + " telah ditambahkan ke dalam antrian.");
-            } else {
-                System.out.println("Gagal menambahkan pasien ke dalam antrian.");
-            }
+            } 
+
         } catch (IllegalArgumentException e) {
             System.out.println("Kesalahan: " + e.getMessage());
         } catch (IllegalStateException e) {
             System.out.println("Kesalahan: " + e.getMessage());
-        } catch (Exception e) {
-            System.out.println("Terjadi kesalahan saat menambahkan pasien: " + e.getMessage());
         }
     }
 
     public void tampilkanAntrian() {
-        try {
-            if (!antrianPasien.isEmpty()) {
-                System.out.println("Antrian saat ini:");
-                for (String namaPasien : antrianPasien) {
-                    System.out.println("- " + namaPasien);
-                }
-            } 
-        } catch (Exception e) {
-            System.out.println("Terjadi kesalahan saat menampilkan antrian: " + e.getMessage());
-        }
+        if (!antrianPasien.isEmpty()) {
+            System.out.println("Antrian saat ini:");
+            for (String namaPasien : antrianPasien) {
+                System.out.println("- " + namaPasien);
+            }
+        } 
     }
 
     public static void main(String[] args) {
