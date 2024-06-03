f36346e390c67bc48b8badd8ebf5ee4bc33542c1
Dhikaaa234 <160991976+Dhikaaa234@users.noreply.github.com>
2024-06-03 16:15:52 +0700

    Add files via upload

---
+++ Project akhir (Sistem Shift Kerja)/shiftKerja/src/main/java/com/mycompany/shiftkerja/user.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.shiftkerja;
+
+/**
+ *
+ * @author ANDHIKA ANANTA PUTRA
+ */
+class user {
+
+    void addUser(String username, String passwordStr) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
