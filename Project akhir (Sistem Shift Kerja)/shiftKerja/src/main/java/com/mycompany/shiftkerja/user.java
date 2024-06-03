66d2eeecca097f72a48793c9686740c25690ca89
Dhikaaa234 <160991976+Dhikaaa234@users.noreply.github.com>
2024-06-03 16:18:35 +0700

    user.java

--- Project akhir (Sistem Shift Kerja)/shiftKerja/src/main/java/com/mycompany/shiftkerja/user.java
+++ Project akhir (Sistem Shift Kerja)/shiftKerja/src/main/java/com/mycompany/shiftkerja/user.java
@@ -2,16 +2,43 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.shiftkerja;
+package User;
+
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  *
  * @author ANDHIKA ANANTA PUTRA
  */
-class user {
-
-    void addUser(String username, String passwordStr) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+public class user {
+    private final List<String> usernames;
+    private final List<String> passwords;
     
+    public user() {
+        this.usernames = new ArrayList<>();
+        this.passwords = new ArrayList<>();
+    }
+
+    public void addUser(String username, String password) {
+        usernames.add(username);
+        passwords.add(password);
+    }
+
+    public boolean isValidUser(String username, String password) {
+        for (int i = 0; i < usernames.size(); i++) {
+            if (usernames.get(i).equals(username) && passwords.get(i).equals(password)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String getUsername(int index) {
+        return usernames.get(index);
+    }
+
+    public String getPassword(int index) {
+        return passwords.get(index);
+    }
 }
